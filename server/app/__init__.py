a3557fedf5b7c19a3c8143627f4469a8948e910a
Andy <andy.ang.2016@smu.edu.sg>
2020-05-19 11:15:48 +0800

    - update package

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response, json, jsonify, request, render_template, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 
-app = Flask(__name__, static_folder='../../static/dist', template_folder='../../static') #static folder
+app = Flask(__name__, static_folder='../../static', template_folder='../../static') #static folder
 app.config['SECRET_KEY'] = "a153246s35746d57f68g9uedtrfyughi98cyas" #secret key
 
 #home page
