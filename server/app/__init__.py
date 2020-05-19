8d6c8ecd6aaff3fbb5d849624b6fbfc88ca304b5
Andy <andy.ang.2016@smu.edu.sg>
2020-05-19 10:21:12 +0800

    - update app structure, pip requirements.txt

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask, Response, json, jsonify, request, render_template, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 
-app = Flask(__name__, static_folder='static', template_folder='static') #static folder
+app = Flask(__name__, static_folder='../../static/dist', template_folder='../../static') #static folder
 app.config['SECRET_KEY'] = "a153246s35746d57f68g9uedtrfyughi98cyas" #secret key
 
-login_manager = LoginManager() #flask login
-login_manager.init_app(app) #initialise app
-
 #home page
+@app.route('/')
 @app.route("/homepage")
 def homepage():
 	return render_template("index.html")
