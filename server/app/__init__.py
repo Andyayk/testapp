be333b6b4f286f1bff7a04274e6ddb80f97cfb5b
Andy <andy.ang.2016@smu.edu.sg>
2020-05-20 09:47:02 +0800

    - update vue app

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask, Response, json, jsonify, request, render_template, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 
-app = Flask(__name__, static_folder='../../static', template_folder='../../static') #static folder
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        variable_start_string='%%',  # Default is '{{', I'm changing this because Vue.js uses '{{' / '}}'
+        variable_end_string='%%',
+    ))
+
+app = CustomFlask(__name__, static_folder='../../static', template_folder='../../static') #static folder
 app.config['SECRET_KEY'] = "a153246s35746d57f68g9uedtrfyughi98cyas" #secret key
 
 #home page
