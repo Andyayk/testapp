7ff9957f158a9804bb315f16288738cf94f77dc6
Andy <andy.ang.2016@smu.edu.sg>
2020-06-15 22:12:54 +0800

    - update spring app

---
+++ Assignment 4/demo/src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+    public void code(){
+        System.out.println("HI");
+    }
+}
