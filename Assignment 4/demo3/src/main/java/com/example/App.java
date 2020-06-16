be45bfa518203ee89ef2b84a00ce5966771a7ef9
Andy <andy.ang.2016@smu.edu.sg>
2020-06-16 22:41:16 +0800

    - update app

--- Assignment 4/demo3/src/main/java/com/example/App.java
+++ Assignment 4/demo3/src/main/java/com/example/App.java
@@ -2,7 +2,10 @@ package com.example;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
+
 
 /**
  * Hello world!
@@ -12,9 +15,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src\\main\\java\\com\\example\\spring.xml"));
+        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src\\main\\java\\com\\example\\spring.xml"));
+        ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
         Alien obj = (Alien)factory.getBean("alien");
-        System.out.println("g");
         obj.code();
     }
 }
