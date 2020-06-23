9dc9e0d00a8cc01068d2d40f53b62b2f52168679
Andy <andy.ang.2016@smu.edu.sg>
2020-06-23 21:43:11 +0800

    - update app

--- Springboot Assignment 1/server/src/main/java/com/example/demo/JobService.java
+++ Springboot Assignment 1/server/src/main/java/com/example/demo/JobService.java
@@ -1,9 +1,17 @@
 package com.example.demo;
-
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 @Service
 public class JobService {
@@ -16,6 +24,24 @@ public class JobService {
         jobs.add(new Job(++jobidCounter, "Police", "Need to manage criminals", "31-12-2020"));
         jobs.add(new Job(++jobidCounter, "Lawyer", "Need to manage law", "31-12-2020"));
         jobs.add(new Job(++jobidCounter, "Doctor", "Need to manage patients", "31-12-2020"));
+
+        // Use a service account
+        InputStream serviceAccount = null;
+        try {
+            serviceAccount = new FileInputStream("path/to/serviceAccount.json");
+
+            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(credentials)
+                    .build();
+            FirebaseApp.initializeApp(options);
+
+            Firestore db = FirestoreClient.getFirestore();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<Job> findAll() {
