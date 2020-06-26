1a758f030f20a25b1f711e9b61a7d7e2acf71441
Andyayk <andy_ang94@hotmail.com>
2020-06-26 17:56:43 +0800

    - update profile page

--- Springboot Assignment 1/client/src/components/UserProfile.vue
+++ Springboot Assignment 1/client/src/components/UserProfile.vue
@@ -2,10 +2,22 @@
     <v-container>
         <h3>Details</h3>
         <v-container>
-            <v-list-item v-for="(value, propertyName, itemIndex) in user" :key="itemIndex">
+            <v-list-item>
                 <v-list-item-content>
-                    <v-list-item-title v-text="value"></v-list-item-title>
-                    <v-list-item-subtitle v-text="propertyName"></v-list-item-subtitle>
+                    <v-list-item-title>{{user.name}}</v-list-item-title>
+                    <v-list-item-subtitle>Name</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+                <v-list-item-content>            
+                    <v-list-item-title>{{user.email}}</v-list-item-title>
+                    <v-list-item-subtitle>Email</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>                    
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>{{user.employeeid}}</v-list-item-title>
+                    <v-list-item-subtitle>Employee Id</v-list-item-subtitle>                                        
                 </v-list-item-content>
             </v-list-item>
         </v-container>
