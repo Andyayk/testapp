606d01a9e9ab9cdb56d4178e1254944c0b0706d0
Andy Ang <andy_ang94@hotmail.com>
2020-09-20 20:57:31 +0800

    - add props

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>HI</h1>
-      <Person />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29}
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>HI</h1>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
--- assignment/src/Person/Person.js
+++ assignment/src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const person = () => {
-    return <p>Person</p>
-}
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm {props.name} and I'm {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
+    )
+};
 
 export default person;
+
