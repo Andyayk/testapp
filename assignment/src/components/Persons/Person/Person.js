5ca26d6a8257e65a13f2e2419eff8f6fa0fd9fd9
Andy Ang <andy_ang94@hotmail.com>
2020-09-28 13:00:36 +0800

    -css module and components

--- assignment/src/components/Persons/Person/Person.js
+++ assignment/src/components/Persons/Person/Person.js
@@ -1,36 +1,17 @@
 import React from 'react';
-//import './Person.css';
-import styled from 'styled-components'
 
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
+import classes from './Person.module.css';
 
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`;
-
-const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
-
-    return (
-        //<div className="Person" style={style}>
-        <StyledDiv>
-            <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}></input>
-        </StyledDiv>
-    )
+const person = props => {
+  return (
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
 };
 
 export default person;
-
--- assignment/src/containers/App.js
+++ assignment/src/containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component, useState} from 'react';
-import './App.css';
-import Person from '../components/Persons/Person/Person'
+import classes from './App.module.css';
+import Persons from '../components/Persons/Persons'
+import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
   state = {
@@ -41,59 +42,22 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
-
     let persons = null;
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-            click={() => this.deletePersonHandler(index)}
-            name={person.name} 
-            age={person.age} 
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)}/>
-          })}
-        </div>        
-      )
-
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
-    }
-
-    const classes = [];
-
-    if (this.state.persons.length <= 2) {
-      classes.push('red'); //classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      classes.push('bold'); //classes = ['red', 'bold']
-    }    
+      persons = <Persons 
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler} 
+            changed={this.nameChangedHandler} />;
+    } 
 
     return (
-      <div className="App">
-        <h1>HI</h1>
-        <p className={classes.join(' ')}>This is really working</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { persons }
+      <div className={classes.App}>
+        <Cockpit 
+          showPersons={this.state.showPersons}
+          persons={this.state.persons} 
+          clicked={this.togglePersonsHandler} />
+        {persons}
       </div>
     );
   }
