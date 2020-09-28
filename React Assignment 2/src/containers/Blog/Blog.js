084a953eefe265664f91f38a0a5ec6c9b49f3650
Andy Ang <andy_ang94@hotmail.com>
2020-09-28 17:41:05 +0800

    -update axios
    Signed-off-by: Andy Ang <andy_ang94@hotmail.com>

--- React Assignment 2/src/containers/Blog/Blog.js
+++ React Assignment 2/src/containers/Blog/Blog.js
@@ -4,18 +4,48 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.splice(0, 4);
+                const updatePosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts: updatePosts});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post 
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}/>;
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
