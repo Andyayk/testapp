4a24380b6afea818542eb1ab2a204425571ce8f8
Andy Ang <andy_ang94@hotmail.com>
2022-09-22 22:11:39 +0800

    - update cypress

--- React Assignment 1/cypress/integration/examples/Test1.js
+++ React Assignment 1/cypress/integration/examples/Test1.js
@@ -2,6 +2,30 @@
 
 describe('My First Test', () => {
     it('Visits the Kitchen Sink', () => {
-      cy.visit('https://example.cypress.io')
+      cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+      cy.get('.search-keyword').type('ca')
+      cy.wait(2000)
+      cy.get('.product:visible').should('have.length', 4)
+
+      //alias
+      cy.get('.products').as('productLocator')
+
+      //parent child chaining
+      cy.get('@productLocator').find('.product').should('have.length', 4)
+
+      //pull second indexed product, find ADD TO CART button
+      cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
+
+      //dynamically find an item using for each
+      cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+        const textVeg = $el.find('h4.product-name').text()
+
+        if (textVeg.includes('Capsicum')) {
+          cy.wrap($el).find('button').click()
+        }
+      })
+
+      //assert if logo text is displayed correctly
+      cy.get(".brand").should("have.text", "GREENKART")
     })
   })
