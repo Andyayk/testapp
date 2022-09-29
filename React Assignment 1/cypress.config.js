f12182dd385ddd83d0e59e29ea84f76aa0e24521
Andy Ang <andy_ang94@hotmail.com>
2022-09-29 17:06:15 +0800

    - update cypress

--- React Assignment 1/cypress.config.js
+++ React Assignment 1/cypress.config.js
@@ -1,22 +1,22 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  chromeWebSecurity: false,
+  // chromeWebSecurity: false,
   defaultCommandTimeout: 8000,
   pageLoadTimeout: 30000,
-  reporter: 'mochawesome',
+  // reporter: 'mochawesome',
   env: {
     url: 'https://rahulshettyacademy.com',
   },
-  retries: {
-    runMode: 1,
-  },
-  projectId: 'a5zbka',
+  // retries: {
+  //   runMode: 1,
+  // },
+  // projectId: 'a5zbka',
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      //return require('./cypress/plugins/index.js')(on, config)
     },
     specPattern: 'cypress/integration/examples/*.js',
   },
