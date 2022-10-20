a65ed64b47df47a8c8cbb307da9c5bd7cfa1542b
Andy Ang <andy_ang94@hotmail.com>
2022-10-20 21:35:16 +0800

    - add BDD

--- React Assignment 1/cypress/integration/examples/BDD/ecommerce.feature
+++ React Assignment 1/cypress/integration/examples/BDD/ecommerce.feature
@@ -2,8 +2,18 @@ Feature: End to end Ecommerce validation
 
     application Regression
 
+    @Regression
     Scenario: : Ecommerce products delivery
-    Given I open Ecommerce Page
-    When I add items to cart
-    And Validate the total prices
-    Then select the country submit and verify Thankyou
+        Given I open ECommerce Page
+        When I add items to cart
+        And Validate the total prices
+        Then select the country submit and verify Thankyou
+
+    @Smoke
+    Scenario: Filling the form to shop
+        Given I open ECommerce Page
+        When I fill the form details
+            | name | gender |
+            | bobz | Male   |
+        Then validate the forms behaviour
+        And select the Shop Page
--- React Assignment 1/cypress/integration/examples/BDD/ecommerce/eCommStepDef.js
+++ React Assignment 1/cypress/integration/examples/BDD/ecommerce/eCommStepDef.js
@@ -1,16 +1,18 @@
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 import HomePage from '../../../../support/pageObjects/HomePage'
 import ProductPage from '../../../../support/pageObjects/ProductPage'
+//cypress run --spec cypress\integration\examples\BDD\*.feature --headed --browser chrome
+//npx cypress-tags run -e TAGS="@Smoke" --headed --browser chrome
 
 const homePage = new HomePage()
 const productPage = new ProductPage()
-
-Given('I open Ecommerce Page', function() {
+let name
+Given('I open ECommerce Page', function () {
     cy.visit(Cypress.env('url') + "/angularpractice/")
 })
 
 // When I add items to cart
-When('I add items to cart', function() {
+When('I add items to cart', function () {
     homePage.getShopTab().click()
     this.data.productName.forEach(function (element) {
         cy.selectProduct(element)
@@ -20,9 +22,9 @@ When('I add items to cart', function() {
 })
 
 // And Validate the total prices
-And('Validate the total prices', function() {
+And('Validate the total prices', function () {
     var sum = 0
-    
+
     cy.get('tr td:nth-child(4) strong').each(($el, index, $list) => {
         const amount = $el.text()
         var res = amount.split(" ")
@@ -40,18 +42,39 @@ And('Validate the total prices', function() {
         expect(Number(total)).to.equal(sum)
 
     })
+})
 
-    //Then select the country submit and verify Thankyou
-    Then('select the country submit and verify Thankyou', function() {
-        cy.contains('Checkout').click()
-        cy.get('#country').type('India')
-        cy.get('.suggestions > ul > li > a').click()
-        cy.get('#checkbox2').click({ force: true })
-        cy.get('input[type="submit"]').click()
-        //cy.get('.alert').should('have.text','Success! Thank you! Your order will be delivered in next few weeks :-).')
-        cy.get('.alert').then(function (element) {
-            const actualText = element.text()
-            expect(actualText.includes("Success")).to.be.true
-        })
+//Then select the country submit and verify Thankyou
+Then('select the country submit and verify Thankyou', function () {
+    cy.contains('Checkout').click()
+    cy.get('#country').type('India')
+    cy.get('.suggestions > ul > li > a').click()
+    cy.get('#checkbox2').click({ force: true })
+    cy.get('input[type="submit"]').click()
+    //cy.get('.alert').should('have.text','Success! Thank you! Your order will be delivered in next few weeks :-).')
+    cy.get('.alert').then(function (element) {
+        const actualText = element.text()
+        expect(actualText.includes("Success")).to.be.true
     })
+})
+
+//When I fill the form details
+When('I fill the form details', function (dataTable) {
+
+    // [bobz , male   ]
+    name = dataTable.rawTable[1][0]
+    homePage.getEditBox().type(dataTable.rawTable[1][0])
+    homePage.getGender().select(dataTable.rawTable[1][1])
+})
+
+// Then validate the forms behaviour
+Then('validate the forms behaviour', function () {
+    homePage.getTwoWayDataBinding().should('have.value', name)
+    homePage.getEditBox().should('have.attr', 'minlength', '2')
+    homePage.getEntrepreneaur().should('be.disabled')
+    Cypress.config('defaultCommandTimeout', 8000)
+})
+// And select the Shop Page
+And('select the Shop Page', function () {
+    homePage.getShopTab().click()
 })
